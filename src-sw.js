78da409ecadbd8378e13d94f83ea2eba5c3cf64f
cmcWebCode40 <infinity_michael@yahoo.com>
2020-01-14 11:41:44 +0100

    working with workbox

---
+++ src-sw.js
@@ -0,0 +1,6 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// custom Adjustments 
+console.log("changes");
+
+workbox.precaching.precachingAndRoute([]);
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+     "globDirectory": "build/",
+     "globPatterns": [
+          "**/*.{jpg,css,png,html,js}"
+     ],
+     "swDest": "build\\sw.js",
+     "swSrc": "src-sw.js"
+};
