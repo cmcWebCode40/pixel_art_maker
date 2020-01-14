34069983555ac1d0352603fc356a914198302c69
cmcWebCode40 <infinity_michael@yahoo.com>
2020-01-14 12:05:24 +0100

    deleted fles

--- serviceWorker.js
+++
@@ -1,8 +0,0 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
-);
-
-// custom Adjustments
-console.log("changes");
-
-workbox.precaching.precachingAndRoute([]);
--- workbox-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globDirectory: "build/",
-  globPatterns: ["**/*.{jpg,css,png,html,js}"],
-  swDest: "build\\sw.js"
-  //   swInject: "serviceWorker.js"
-};
